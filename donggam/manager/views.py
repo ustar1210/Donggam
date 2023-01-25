c1815b06864e7f12ad52b5ed03fd110e99617149
ustar1210 <ays991210@naver.com>
2023-01-26 00:47:21 +0900

    fix - 관리자가 단체투어 신청 시 에러 해결

--- donggam/manager/views.py
+++ donggam/manager/views.py
@@ -72,41 +72,43 @@ def admin_save(request):
         return redirect('manager:login')
 
 def group_form(request, reservation_id):
-    instance = get_object_or_404(Reservation, pk=reservation_id)
-    if instance.status == '1':
-        status = '신청대기'
-    elif instance.status == '2':
-        status = '검토중'
-    elif instance.status == '3':
-        status = '승인완료'
-    date = str(instance.date).split('-')
-    date = date[0] + '년 ' + date[1] + '월 ' + date[2] + '일'
-    time = instance.time + ':00'
-    datetime = date + ' / ' + time
-    if instance.grade == 'n':
-        grade = '기타'
-    else :
-        grade = instance.grade + '학년'
-    if instance.major == 0:
-        major = '공통'
-    elif instance.major == 1:
-        major = '문과'
-    elif instance.major == 2:
-        major = '이과'
-    length = str(instance.length)+'0분'
+    try:
+        instance = get_object_or_404(Reservation, pk=reservation_id)
+        if instance.status == '1':
+            status = '신청대기'
+        elif instance.status == '2':
+            status = '검토중'
+        elif instance.status == '3':
+            status = '승인완료'
+        date = str(instance.date).split('-')
+        date = date[0] + '년 ' + date[1] + '월 ' + date[2] + '일'
+        time = instance.time + ':00'
+        datetime = date + ' / ' + time
+        if instance.grade == 'n':
+            grade = '기타'
+        else :
+            grade = instance.grade + '학년'
+        if instance.major == 0:
+            major = '공통'
+        elif instance.major == 1:
+            major = '문과'
+        elif instance.major == 2:
+            major = '이과'
+        length = str(instance.length)+'0분'
 
-    places = Place.objects.all()
-    return render(request, 'manager/groupform_admin.html', 
-    {
-        'reservation' : instance,
-        'status': status,
-        'datetime': datetime,
-        'grade': grade,
-        'major': major,
-        'length': length,
-        'places': places,
-    })
-
+        places = Place.objects.all()
+        return render(request, 'manager/groupform_admin.html', 
+        {
+            'reservation' : instance,
+            'status': status,
+            'datetime': datetime,
+            'grade': grade,
+            'major': major,
+            'length': length,
+            'places': places,
+        })
+    except:
+        return redirect('calender:reservation_edit', reservation_id = reservation_id)
 
 def group_confirm(request, reservation_id):
     if request.user.is_authenticated:
