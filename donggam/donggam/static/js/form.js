34dd45ac48c2daa7c91e5967dc4b481ed9945a24
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-01-30 03:36:32 +0900

    feat : 정기캠퍼스투어 관리자 공지 비동기처리

--- donggam/donggam/static/js/form.js
+++ donggam/donggam/static/js/form.js
@@ -52,6 +52,54 @@ function realDel() {
     return confirm("삭제하시겠습니까?");
 }
 
-function realConf(){
+function realConf() {
     return confirm("저장하시겠습니까?");
 }
+function realConf() {
+    return confirm("수정하시겠습니까?");
+}
+
+// 정기 공지 수정 버튼 클릭시
+$(function () {
+    // type 이 radio 이고 id adult 인 input 을 click 했을 경우
+    $('input[type="submit"][id="edit_notice"]').on("click", function () {
+        // startSetting 에 checked된 radio button의 value 값을 넣는다.
+        var startSetting = $('input[type="submit"][id="edit_notice"]').val();
+        // startSetting이 later인 경우 style display를 flex로 변경한다.
+        // d이면 미성인
+        if (startSetting == "수정") {
+            $('input[type="submit"][id="edit_notice"]').attr(
+                "value",
+                "수정완료"
+            );
+            $("#add_notice").css("display", "none");
+            $("#notice_done_edit").css("display", "none");
+            $("#notice_yet_edit").css("display", "inline");
+            // 그외의 경우 style display를 none 으로 변경한다.
+        } else {
+            // 수정으로 글 변경
+            $('input[type="submit"][id="edit_notice"]').attr("value", "수정");
+            alert("수정 완료 됐습니다.");
+        }
+    });
+});
+
+// 정기 공지 추가하기 버튼 클릭시
+$(function () {
+    // type 이 radio 이고 id adult 인 input 을 click 했을 경우
+    $('input[type="submit"][id="add_notice"]').on("click", function () {
+        // startSetting 에 checked된 radio button의 value 값을 넣는다.
+        $("#add_notice").css("display", "none");
+        $("#edit_notice").css("display", "none");
+        $("#add_info").css("display", "block");
+    });
+});
+
+$(function () {
+    $('input[type="submit"][id="save_notice"]').on("click", function () {
+        // startSetting 에 checked된 radio button의 value 값을 넣는다.
+        $("#add_notice").css("display", "inline");
+        $("#edit_notice").css("display", "inline");
+        $("#add_info").css("display", "none");
+    });
+});
